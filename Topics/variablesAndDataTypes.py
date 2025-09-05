be3276458a88534b0d96f94da81b02a541796955
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-09-05 08:03:28 -0300

    feat Add topic variables and DataTypes

---
+++ Topics/variablesAndDataTypes.py
@@ -0,0 +1,34 @@
+#print("Hello world")
+
+# Váriaveis
+person_name = "José"
+person_age = "80"
+
+# utilizando Print para criar uma história
+print("Havia um homem chamado  " + person_name + ",")
+print("ele tinha " + person_age +  " anos.")
+print("Ele realmente gostava de se chamar " + person_name + ",")
+print("porém nao gostava de ter " + person_age + " anos.")
+
+# Atribuindo novos valores de variáveis no meio da historia
+
+print("Havia um homem chamado  " + person_name + ",")
+print("ele tinha " + person_age +  " anos.")
+
+# Novos valores de variáveis.
+person_name = "Pedro"
+person_age = "24"
+
+#Continua história porem com as variáveis novas.
+print("Ele realmente gostava de se chamar " + person_name + ",")
+print("porém nao gostava de ter " + person_age + " anos.")
+
+#Tipos de dados
+variavel_string = "texto" #variável tipo string
+variavel_number = 50  # variável tipo number (int, Numero inteiro)
+variavel_number_float = 1.73 # variável tipo number (float, Numeros decimais)
+variavel_complex_number = 3 + 5j # variável tipo number complex → números complexos (pouco usado, mas existe)
+váriavel_boolean = True #variável tipo boolean true or false, Inicia-se com letra maiúscula "T"rue , "F"alse
+#ex:
+pedro_is_man = True # Boolean tipo True
+pedro_is_woman = False # Boolean tipo false
