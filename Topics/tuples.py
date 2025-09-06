08289ebe5749db609c8bf7ead4bf9d85bfcac7f3
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-09-06 04:09:46 -0300

    feat: tuples/Tuplas

---
+++ Topics/tuples.py
@@ -0,0 +1,23 @@
+# Tupla é um tipo de dado ( estrutura, é basicamente um container onde podemos armazenar vários valores )
+#Semelhante a uma lista
+
+#Diferenças de uma tupla em relação a uma lista:
+
+#TUPLAS NAO PODEM SER MUDADAS!!!!!!!!! É UMA "CONSTANTE" IMUTÁVEL
+
+
+#sempre para criar uma tupla segue esse padrao = ()
+# cordenadas = ()
+
+cordenadas = (4,5)
+#             0,1    -> index
+print(cordenadas[1])  # resultado -> "5" pois esta no index 1 da tupla
+
+
+ #em usos praticos no dia a dia, usa-se para armazenar algo que o dado é imutavel.
+
+ #pode se fazer varias tuplas dentro de uma lista
+ #ex:
+
+cordenadas_list = [(4, 5), (6, 7), (8, 9)]
+#                [ tupla   tupla   tupla  ] lista
