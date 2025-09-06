5d83549b863186cc06a1a6d27519cab9390cacc5
pedroferreirasousa <pepeferreiras.pf@gmail.com>
2025-09-06 13:50:30 -0300

    feat: adicionando Topic whileLoop

---
+++ Topics/whileLoop.py
@@ -0,0 +1,27 @@
+#LOOPS WHILE -> É BASICAMENTE UMA ESTRUTURA EM PYTHON QUE NOS PERMITE PERCORRER E EXECUTAR UM BLOCO DE CODIGO VARIAS VEZES
+#Posso coloca-lo e ele repetiria em looping e pararia apenas quando a contição passada for concretizada
+
+
+#Noções BÁSICAS de como funciona:
+
+i = 1
+
+while i <= 10: #ENQUANtO "CONDIÇAO": nesse caso "Enquanto i for menor ou igual a 10"
+  print(i)
+  i += 1  # abreviando a logica de i = i + 1.
+
+
+print("Finalizado o looping")
+
+#RESULTADO =
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# Finalizado o looping
